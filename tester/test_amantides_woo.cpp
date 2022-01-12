da9b044a15c929377b0e998340262fb62d685f09
Bjorn Wehlin <bjorn.wehlin@gmail.com>
2022-01-12 11:35:58 +0100

    Added citation

--- tester/test_amantides_woo.cpp
+++ tester/test_amantides_woo.cpp
@@ -2,6 +2,11 @@
 
 #include "../RayOfLife/amantides_woo.cuh"
 
+// Here we are testing that our implementation gives the same results as an existing implementation.
+// 
+// As a reference, we used Jesus P. Mena-Chalco's MATLAB implementation that is available
+// at https://mathworks.com/matlabcentral/fileexchange/26852-a-fast-voxel-traversal-algorithm-for-ray-tracing
+
 TEST(AmantidesWoo, StartAtOrigin) 
 {
   auto state = rol::initAmantidesWoo(make_float3(0.f, 0.f, 0.f), make_float3(0.3f, 0.5f, 0.7f));
