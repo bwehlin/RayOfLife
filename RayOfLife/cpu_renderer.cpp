8f8f099ecfc427ffb88d6dfd94c90c3b5a3d6a1e
Bjorn Wehlin <bjorn.wehlin@gmail.com>
2022-01-15 21:13:11 +0100

    Citations

--- RayOfLife/cpu_renderer.cpp
+++ RayOfLife/cpu_renderer.cpp
@@ -58,6 +58,30 @@ rol::CpuRenderer::produceFrame(const Game& game, const Camera& camera,
   }
 }
 
+/*
+This file started its life as a C++ port of the 'very simple ray tracing engine'
+available at https://gist.github.com/rossant/6046463, which is licensed as follows:
+
+MIT License
+Copyright (c) 2017 Cyrille Rossant
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 fptype3
 rol::CpuRenderer::renderPixel(itype ix, itype iy, 
   fptype x, fptype y, 
--- RayOfLife/intersect.cuh
+++ RayOfLife/intersect.cuh
@@ -10,6 +10,30 @@
 #include <cuda/std/limits>
 #include <cuda/std/utility>
 
+/*
+This file started its life as a C++ port of the 'very simple ray tracing engine'
+available at https://gist.github.com/rossant/6046463, which is licensed as follows:
+
+MIT License
+Copyright (c) 2017 Cyrille Rossant
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 namespace rol
 {
   struct RayIntersection
@@ -78,7 +102,6 @@ namespace rol
 
     auto specular = scene.sphereSpecularC * rol::max(dot(intersection.normal, normalize(toLight + toCameraOrigin)), 0.f);
 
-    // TODO: pow GPU
     specular = pow(specular, scene.specularK);
     intersection.color += specular * scene.lightColor;
 
